0f9b6da347491c56ab862e42755dcf4d7daa0928
SirajiNouha <nouhailasiraji5@gmail.com>
2023-08-16 05:45:36 -0700

    second

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,12 @@
 #include "main.h"
-int main(void){
-	positive_or_negative(0);
-	return(0);
+/**
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
