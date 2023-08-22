248cb0fb832fc28c6ca106f98f2486e18e4c1921
SirajiNouha <nouhailasiraji5@gmail.com>
2023-08-22 07:32:13 -0700

    second

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+int _putchar(char c);
+void _puts(char *str);
+void print_rev(char *s);
+
